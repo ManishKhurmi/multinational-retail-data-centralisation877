992b7c7069a9686d653b1e72c4d2e7c58c3fe59c
ManishKhurmi <manish_khurmi@hotmail.co.uk>
2024-11-14 00:37:51 +0000

    created function to read db connections

--- database_utils.py
+++ database_utils.py
@@ -1,4 +1,36 @@
+import yaml 
+
 class DatabaseConnector:
     ''' connects with the database to upload the data'''
-    def __init__(self):
-        self = # connection etc 
+    # step 2 
+    @staticmethod
+    def read_db_creds(file_path):
+        """Reads the database credentials from a YAML file and returns them as a dictionary."""
+        try:
+            with open(file_path, 'r') as file:
+                creds = yaml.safe_load(file)
+            return creds
+        except Exception as e:
+            print("Error reading the credentials file:", e)
+            return None
+        
+    
+# Example usage
+db_connect = DatabaseConnector()
+creds = db_connect.read_db_creds('db_creds.yaml')
+print(creds)  # This should print out the dictionary of credentials
+
+# Testing 
+
+
+
+
+# # connect to sales_data
+# import psycopg2
+# with psycopg2.connect(host='localhost', user='postgres', password='ginger', dbname='sales_data', port=5432) as conn:
+#     with conn.cursor() as cur:
+#         cur.execute("""SELECT table_name FROM information_schema.tables
+#        WHERE table_schema = 'public'""")
+#         for table in cur.fetchall():
+#             print(table)
+
